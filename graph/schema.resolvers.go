41fa1ea8cc4ddbae6767c82a2c74a91e94ccce6e
zub <zub@pop-os.localdomain>
2021-05-09 23:01:23 +0700

    add maximum 4 new project a month logic

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -6,6 +6,7 @@ package graph
 import (
 	"context"
 	"errors"
+	"fmt"
 	"os"
 	"time"
 
@@ -86,6 +87,43 @@ func (r *mutationResolver) CreateProject(ctx context.Context, input model.NewPro
 		return nil, err
 	}
 
+	db := db.DbConn()
+
+	// Check whether the user has created the project 4 times within a month.
+	// Count from the last project's created date minus a month interval.
+	var lastProjectCreated model.Project
+	if err := db.Last(&lastProjectCreated).Error; err != nil {
+		return nil, err
+	}
+
+	lastProjectCreatedDate, err := time.Parse(time.RFC3339, lastProjectCreated.CreatedAt)
+	if err != nil {
+		return nil, err
+	}
+
+	aMonthInterval := lastProjectCreatedDate.AddDate(0, -1, 0)
+
+	var aMonthIntervalData []model.Project
+	err = db.Where("created_at >= ? AND created_at <= ?", aMonthInterval, lastProjectCreatedDate).
+		Order("ID ASC").
+		Find(&aMonthIntervalData).Error
+	if err != nil {
+		return nil, err
+	}
+
+	nextMonth, err := time.Parse(time.RFC3339, aMonthIntervalData[0].CreatedAt)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(aMonthIntervalData) >= 4 {
+		return nil, fmt.Errorf(
+			"you are already created 4 projects within a month, kindly please wait until %v to create a new project",
+			nextMonth.AddDate(0, 1, 0),
+		)
+	}
+	// End of counter project checking
+
 	project := &model.Project{
 		CreatorID: int(token.ID),
 		Name:      input.Name,
@@ -96,7 +134,6 @@ func (r *mutationResolver) CreateProject(ctx context.Context, input model.NewPro
 		UpdatedAt: time.Now().Format(time.RFC3339),
 	}
 
-	db := db.DbConn()
 	result := db.Select("CreatorID", "Name", "Desc", "Price", "Deadline", "CreatedAt", "UpdatedAt").
 		Create(&project)
 	if result.Error != nil {
